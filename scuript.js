3e154b633ca5d4c9f02c596b7e342648abac7106
Mostolo <122951205+Mostolo@users.noreply.github.com>
2023-01-26 02:52:18 +0100

    Add files via upload

---
+++ scuript.js
@@ -0,0 +1,155 @@
+function FuncSportello() {
+    var uno = document.getElementById("Sportello")
+    var due = document.getElementById("Cubi")
+    var tre = document.getElementById("Cerchi")
+    var quattro = document.getElementById("Emoticon")
+    var cinque = document.getElementById("Cupido")
+    var sei = document.getElementById("Cuori")
+
+    {
+    due.style.display = "none"
+    tre.style.display = "none"
+    quattro.style.display = "none"
+    cinque.style.display = "none"
+    sei.style.display = "none"
+    }
+
+    if (uno.style.display === "block") {
+        uno.style.display = "none"
+    } else {
+        uno.style.display = "block"
+    }
+}
+
+function FuncCubi() {
+    var uno = document.getElementById("Cubi")
+    var unoo = document.getElementById("Sportello")
+    var tre = document.getElementById("Cerchi")
+    var quattro = document.getElementById("Emoticon")
+    var cinque = document.getElementById("Cupido")
+    var sei = document.getElementById("Cuori")
+
+    {
+    unoo.style.display = "none"
+    tre.style.display = "none"
+    quattro.style.display = "none"
+    cinque.style.display = "none"
+    sei.style.display = "none"
+    }
+
+    if (uno.style.display === "block") {
+        uno.style.display = "none"
+    } else {
+        uno.style.display = "block"
+    }
+}
+
+function FuncCerchi() {
+    var uno = document.getElementById("Cerchi")
+    var unoo = document.getElementById("Sportello")
+    var due = document.getElementById("Cubi")
+    var quattro = document.getElementById("Emoticon")
+    var cinque = document.getElementById("Cupido")
+    var sei = document.getElementById("Cuori")
+
+    {
+    unoo.style.display = "none"
+    due.style.display = "none"
+    quattro.style.display = "none"
+    cinque.style.display = "none"
+    sei.style.display = "none"
+    }
+
+    if (uno.style.display === "block") {
+        uno.style.display = "none"
+    } else {
+        uno.style.display = "block"
+    }
+}
+
+function FuncEmoticon() {
+    var uno = document.getElementById("Emoticon")
+    var unoo = document.getElementById("Sportello")
+    var due = document.getElementById("Cubi")
+    var tre = document.getElementById("Cerchi")
+    var cinque = document.getElementById("Cupido")
+    var sei = document.getElementById("Cuori")
+
+    {
+    unoo.style.display = "none"
+    due.style.display = "none"
+    tre.style.display = "none"
+    cinque.style.display = "none"
+    sei.style.display = "none"
+    }
+
+    if (uno.style.display === "block") {
+        uno.style.display = "none"
+    } else {
+        uno.style.display = "block"
+    }
+}
+
+function FuncCupido() {
+    var uno = document.getElementById("Cupido")
+    var unoo = document.getElementById("Sportello")
+    var due = document.getElementById("Cubi")
+    var tre = document.getElementById("Cerchi")
+    var quattro = document.getElementById("Emoticon")
+    var sei = document.getElementById("Cuori")
+
+    {
+    unoo.style.display = "none"
+    due.style.display = "none"
+    tre.style.display = "none"
+    quattro.style.display = "none"
+    sei.style.display = "none"
+    }
+
+    if (uno.style.display === "block") {
+        uno.style.display = "none"
+    } else {
+        uno.style.display = "block"
+    }
+}
+
+function FuncCuori() {
+    var uno = document.getElementById("Cuori")
+    var unoo = document.getElementById("Sportello")
+    var due = document.getElementById("Cubi")
+    var tre = document.getElementById("Cerchi")
+    var quattro = document.getElementById("Emoticon")
+    var cinque = document.getElementById("Cupido")
+
+    {
+    unoo.style.display = "none"
+    due.style.display = "none"
+    tre.style.display = "none"
+    quattro.style.display = "none"
+    cinque.style.display = "none"
+    }
+
+    if (uno.style.display === "block") {
+        uno.style.display = "none"
+    } else {
+        uno.style.display = "block"
+    }
+}
+
+function Chiudi() {
+    var unoo = document.getElementById("Sportello")
+    var due = document.getElementById("Cubi")
+    var tre = document.getElementById("Cerchi")
+    var quattro = document.getElementById("Emoticon")
+    var cinque = document.getElementById("Cupido")
+    var sei = document.getElementById("Cuori")
+
+    {
+    unoo.style.display = "none"
+    due.style.display = "none"
+    tre.style.display = "none"
+    quattro.style.display = "none"
+    cinque.style.display = "none"
+    sei.style.display = "none"
+    }
+}
